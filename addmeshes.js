90220da035022cda329dfbaac5c1caa03abc77ae
xara <66925451+xaralu@users.noreply.github.com>
2024-03-28 19:11:55 -0400

    first

--- addmeshes.js
+++ addmeshes.js
@@ -11,7 +11,7 @@ export const addBoilerPlateMesh = () => {
 export function addStandardMesh () {
     const box = new THREE.BoxGeometry(1, 1, 1)
     const boxMaterial = new THREE.MeshStandardMaterial({color: 0x00ff00})
-    const boxMesh = new THREE.Mesh(box, boxMaterial) 
+    const boxMesh = new THREE.Mesh(box, boxMaterial)
     boxMesh.position.set(-2, 0, 0)
     return boxMesh;
 }
